687d37344f9fd98ee6d39322e5ff5dbb57e44835
AceAtomz <93909209+AceAtomz@users.noreply.github.com>
2022-10-23 17:16:53 +0200

    serial implementation and output

--- scan/plot.py
+++ scan/plot.py
@@ -4,14 +4,14 @@ if __name__ == '__main__':
 
     filepath = './scan.out'
     names = ["Serial", "OpenMP" , "MPI"]  
-    x = list(range(1000, 20001, 1000))  # this is the range of input sizes tested
+    x = list(range(1000, 50001, 1000))  # this is the range of input sizes tested
     with open(filepath, 'r') as file:
         for i, line in enumerate(file):
-            y = list(map(int, line.split(',')))
+            y = list(map(float, line.split(',')))
             plt.plot(x, y, label=names[i])
 
     plt.xlabel('Input size')
-    plt.ylabel('Time taken (ns)')
+    plt.ylabel('Time taken (ms)')
     plt.title('Comparison of Prefix Sum Procedures')
     plt.legend()
     plt.savefig('scan_comparison.pdf')
--- scan/scan.c
+++ scan/scan.c
@@ -1,31 +1,29 @@
 /* File:     scan.c
- * Purpose:  Serial implementation of the Scan operation/All-prefix-sums operation (Blelloch 1990)
+ * Purpose:  Serial implementation of the Prefix Sum operation (Blelloch 1990)
  *
  * Input:    
- * Output:   Estimate of the integral from a to b of f(x)
- *           using the trapezoidal rule and n trapezoids.
+ * Output:   If the scan was validated or not.
+ *           scan.out holds average times for plotting
  *
  * Compile:  gcc -g -Wall -fopen scan.c -o scan
  * Run:      ./scan
  *
- * Algorithm: (See Lec 8 slides) TODO: Fix algorithm description
- *    1.  Each process calculates "its" interval of integration.
- *    2.  Each process estimates the integral of f(x)
- *        over its interval using the trapezoidal rule.
- *    3a. Each process != 0 sends its integral to 0.
- *    3b. Process 0 sums the calculations received from
- *        the individual processes and prints the result.
- *
+ * Algorithm:
+ *    1.  Given a set of elements [a0,a1,··· ,an−1].
+ *    2.  The scan operation returns the set [a0,(a0+a1),···,(a0+a1 +···+an−1)].
+ *    eg. The input set [2,1,4,0,3,7,6,3],
+ *        yields [2,3,7,7,10,17,23,26].
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
 
-int size = 1000;
-int maxsize = 20000;
-double times[20];
-int runs = 5;
+#define size 1000
+#define maxsize 50000
+#define n_times (maxsize/size)
+#define runs 5
+double times[n_times];
 FILE *fptr;
 
 void scan(int out[], int in[], int N){
@@ -37,30 +35,36 @@ void scan(int out[], int in[], int N){
 }
 
 void printTimes(){
-    //TODO: Write times to a .out file
-    fptr = fopen("C:\\Users\\User\\Documents\\GitHub\\PC-Assignment\\1.Parallel_Scan\\scan.out","a");
+    fptr = fopen("scan.out","w");
 
     if(fptr == NULL){
         printf("Error!");   
         exit(1);
     }
-
-    char* out = "";
-    for(int i=0;i<20;i++){
-        snprintf(out, 20, "%f", times[i]);
-        if(i!=19) out+=',';
+    
+    for(int i=0;i<n_times;i++){
+        times[i] = times[i]*1000;
+        fprintf(fptr,"%f",times[i]); //print to file
+        if(i!=n_times-1) fprintf(fptr,",");
     }
-
-    fprintf(fptr,"%s",out); //print to file
-    printf("Done!");  
     fclose(fptr);
+}
+
+void validate_scan(int n, int *data){
+	int i;
+	for(i=0;i<n-1;i++){
+		if(data[i] > data[i+1]){
+			printf("Validata failed. \n");
+		}
+	}
+	printf("Validate passed.\n");
 }
 
 int main(int argc, char *argv[]) {
     int times_counter=0;
     
     if(argc != 1){
-		printf("Wrong input parameters\nscan.out\n"); //must specify .out file
+		printf("Too many input parameters\n"); //must not have any parameters
 		return 1;
 	}
 
@@ -68,7 +72,8 @@ int main(int argc, char *argv[]) {
         double total_time=0, average_time=0;
         srand(n); //sets seed for random generation
 
-        int data[n], outData[n];
+        int *data=(int*)malloc(sizeof(int)*n);
+        int *outData=(int*)malloc(sizeof(int)*n);
         for(int i=0; i<n; i++){     //generates sudo-random numbers
             data[i]=1.1*rand()*5000/RAND_MAX;
         }
@@ -77,7 +82,7 @@ int main(int argc, char *argv[]) {
         for(int i=0;i<runs;i++){
             double start_time=omp_get_wtime(); //get start time
             
-            scan(outData, data, n);
+            scan(outData, data, n); //performs the prefix sum operation
 
             double finish_time=omp_get_wtime(); //get finish time
             total_time+= finish_time-start_time;    //add to total running time
@@ -85,6 +90,9 @@ int main(int argc, char *argv[]) {
         average_time = total_time / runs;   //calc average time of algorithm
         times[times_counter] = average_time; //store average times
         
+        if(n==maxsize) validate_scan(n, outData); //validate that the prefix sum works correctly
+        free(data);
+        free(outData);
         times_counter++;
     }
 
