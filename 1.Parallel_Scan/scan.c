e2cf24d6e57bae4a54a5b81709308ec228da7186
AceAtomz <93909209+AceAtomz@users.noreply.github.com>
2022-10-23 15:56:40 +0200

    trying to out to file

--- 1.Parallel_Scan/scan.c
+++ 1.Parallel_Scan/scan.c
@@ -8,7 +8,7 @@
  * Compile:  gcc -g -Wall -fopen scan.c -o scan
  * Run:      ./scan
  *
- * Algorithm: (See Lec 8 slides)
+ * Algorithm: (See Lec 8 slides) TODO: Fix algorithm description
  *    1.  Each process calculates "its" interval of integration.
  *    2.  Each process estimates the integral of f(x)
  *        over its interval using the trapezoidal rule.
@@ -26,6 +26,7 @@ int size = 1000;
 int maxsize = 20000;
 double times[20];
 int runs = 5;
+FILE *fptr;
 
 void scan(int out[], int in[], int N){
     out[0] = in[0];
@@ -33,6 +34,26 @@ void scan(int out[], int in[], int N){
     for(int i=1; i<N; i++) {
         out[i] = in[i] + out[i-1];
     }
+}
+
+void printTimes(){
+    //TODO: Write times to a .out file
+    fptr = fopen("C:\\Users\\User\\Documents\\GitHub\\PC-Assignment\\1.Parallel_Scan\\scan.out","a");
+
+    if(fptr == NULL){
+        printf("Error!");   
+        exit(1);
+    }
+
+    char* out = "";
+    for(int i=0;i<20;i++){
+        snprintf(out, 20, "%f", times[i]);
+        if(i!=19) out+=',';
+    }
+
+    fprintf(fptr,"%s",out); //print to file
+    printf("Done!");  
+    fclose(fptr);
 }
 
 int main(int argc, char *argv[]) {
@@ -47,7 +68,7 @@ int main(int argc, char *argv[]) {
         double total_time=0, average_time=0;
         srand(n); //sets seed for random generation
 
-        int data[n];
+        int data[n], outData[n];
         for(int i=0; i<n; i++){     //generates sudo-random numbers
             data[i]=1.1*rand()*5000/RAND_MAX;
         }
@@ -56,13 +77,16 @@ int main(int argc, char *argv[]) {
         for(int i=0;i<runs;i++){
             double start_time=omp_get_wtime(); //get start time
             
+            scan(outData, data, n);
+
             double finish_time=omp_get_wtime(); //get finish time
             total_time+= finish_time-start_time;    //add to total running time
         }
         average_time = total_time / runs;   //calc average time of algorithm
-        times[times_counter] = average_time;
-
-        //scan();
+        times[times_counter] = average_time; //store average times
+        
         times_counter++;
     }
+
+    printTimes();
 }
