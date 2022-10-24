c828552186f063664e6c0291bac862b40038862c
AceAtomz <93909209+AceAtomz@users.noreply.github.com>
2022-10-24 17:28:06 +0200

    problem 1 complete

--- scan/plot.py
+++ scan/plot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-
+    
     filepath = './scan/scan.out'
     names = ["Serial", "OpenMP T4", "MPI T4"]
     x = list(range(1000, 50001, 1000))  # this is the range of input sizes tested
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
     plt.xlabel('Input size')
     plt.ylabel('Time taken (ms)')
-    plt.title('Serial implementation of Prefix Sum operation')
+    plt.title('Prefix Sum operation comparisons')
     plt.legend()
-    plt.savefig('serial_scan.png')
+    plt.savefig('./scan/scan_comparison.png')
     plt.show()
